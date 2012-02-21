7769096be84135ca05433e9c28c1361c0db29c83
reiserford <kolpak@stack.net>
2012-02-21 13:55:24 +0400

    add support Style->color "rgb" attribute, fix bug in to merged cells collection, add as_HTML and cell_as_HTML methods

--- lib/SimpleXlsx.pm
+++ lib/SimpleXlsx.pm
@@ -1,5 +1,5 @@
 package SimpleXlsx;
-
+    
 use strict;
 use warnings;
 use Archive::Zip qw( :ERROR_CODES );
@@ -15,11 +15,13 @@ our @ISA = qw(Exporter);
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 
-our @EXPORT_OK = ( 'parse' );
+our @EXPORT_OK = ( 'parse', 'as_HTML' );
 
 our $VERSION = '0.05';
 
-# Preloaded methods go here.
+our %indexedColors2ARGB = (0=>'00000000', 1=>'00FFFFFF', 2=>'00FF0000', 3=>'0000FF00', 4=>'000000FF', 5=>'00FFFF00', 6=>'00FF00FF', 7=>'0000FFFF', 8=>'00000000', 9=>'00FFFFFF', 10=>'00FF0000', 11=>'0000FF00', 12=>'000000FF', 13=>'00FFFF00', 14=>'00FF00FF', 15=>'0000FFFF', 16=>'00800000', 17=>'00008000', 18=>'00000080', 19=>'00808000', 20=>'00800080', 21=>'00008080', 22=>'00C0C0C0', 23=>'00808080', 24=>'009999FF', 25=>'00993366', 26=>'00FFFFCC', 27=>'00CCFFFF', 28=>'00660066', 29=>'00FF8080', 30=>'000066CC', 31=>'00CCCCFF', 32=>'00000080', 33=>'00FF00FF', 34=>'00FFFF00', 35=>'0000FFFF', 36=>'00800080', 37=>'00800000', 38=>'00008080', 39=>'000000FF', 40=>'0000CCFF', 41=>'00CCFFFF', 42=>'00CCFFCC', 43=>'00FFFF99', 44=>'0099CCFF', 45=>'00FF99CC', 46=>'00CC99FF', 47=>'00FFCC99', 48=>'003366FF', 49=>'0033CCCC', 50=>'0099CC00', 51=>'00FFCC00', 52=>'00FF9900', 53=>'00FF6600', 54=>'00666699', 55=>'00969696', 56=>'00003366', 57=>'00339966', 58=>'00003300', 59=>'00333300', 60=>'00993300', 61=>'00993366', 62=>'00333399', 63=>'00333333');
+our %CellDataTypes      = ('b'=>'Boolean', 'd'=>'Date', 'e'=>'Error', 'inlineStr'=>'Inline String', 'n'=>'Number', 's'=>'Shared String', 'str'=>'String');
+our %HtmlBorderStyles   = (map {$_=>1} qw(none hidden dotted dashed solid double groove ridge inset outset));
 
 sub new
 {
@@ -27,8 +29,6 @@ sub new
 
   my $self = {};
   $self->{zip} = Archive::Zip->new();
-  $self->{indexedColors2ARGB} = {0=>'00000000', 1=>'00FFFFFF', 2=>'00FF0000', 3=>'0000FF00', 4=>'000000FF', 5=>'00FFFF00', 6=>'00FF00FF', 7=>'0000FFFF', 8=>'00000000', 9=>'00FFFFFF', 10=>'00FF0000', 11=>'0000FF00', 12=>'000000FF', 13=>'00FFFF00', 14=>'00FF00FF', 15=>'0000FFFF', 16=>'00800000', 17=>'00008000', 18=>'00000080', 19=>'00808000', 20=>'00800080', 21=>'00008080', 22=>'00C0C0C0', 23=>'00808080', 24=>'009999FF', 25=>'00993366', 26=>'00FFFFCC', 27=>'00CCFFFF', 28=>'00660066', 29=>'00FF8080', 30=>'000066CC', 31=>'00CCCCFF', 32=>'00000080', 33=>'00FF00FF', 34=>'00FFFF00', 35=>'0000FFFF', 36=>'00800080', 37=>'00800000', 38=>'00008080', 39=>'000000FF', 40=>'0000CCFF', 41=>'00CCFFFF', 42=>'00CCFFCC', 43=>'00FFFF99', 44=>'0099CCFF', 45=>'00FF99CC', 46=>'00CC99FF', 47=>'00FFCC99', 48=>'003366FF', 49=>'0033CCCC', 50=>'0099CC00', 51=>'00FFCC00', 52=>'00FF9900', 53=>'00FF6600', 54=>'00666699', 55=>'00969696', 56=>'00003366', 57=>'00339966', 58=>'00003300', 59=>'00333300', 60=>'00993300', 61=>'00993366', 62=>'00333399', 63=>'00333333'};
-  $self->{CellDataTypes}      = {'b'=>'Boolean', 'd'=>'Date', 'e'=>'Error', 'inlineStr'=>'Inline String', 'n'=>'Number', 's'=>'Shared String', 'str'=>'String'};
   
   return bless $self, $package;
 }
@@ -70,47 +70,27 @@ sub parse
     my $sComments  = $self->getSheetComments($sRelations);
 
     my(%worksheet);
-    $worksheet{'Rows'} = [];
-    $worksheet{'Data'} = {};
+    $worksheet{'Rows'}  = [];
+    $worksheet{'Data'}  = {};
     $worksheet{'Merge'} = {};
-    
-    my($contents) = $file->contents();
-    
+        
     my($name) = basename($file->fileName());
     $name =~ s/\.xml$//;
     
+    my($contents) = $file->contents();
     my($xml) = new XML::Simple;
     my($data) = $xml->XMLin($contents);
     
-    my($sData) = $data->{'sheetData'}->{'row'};
-    my($sMerge) = $data->{'mergeCells'}->{'mergeCell'};
-    
-    my(%merge);
-    for my $mc (@{$sMerge})
-    {
-      my($from, $to) = split(':', $mc->{'ref'});
-      
-      $from =~ /([a-zA-Z]+)([0-9]+)/;
-      my($col1, $row1) = ($1, $2);
-      
-      $to =~ /([a-zA-Z]+)([0-9]+)/;
-      my($col2, $row2) = ($1, $2);
-      
-      $merge{$row1} =
-      {
-        'From' => { 'Row' => $row1, 'Column' => $col1 },
-        'To' => { 'Row' => $row2, 'Column' => $col2 }
-      };
-    }
-    
-    
-    if ($sData) {
-      my(@tcol);
+    my $sData = $data->{'sheetData'}->{'row'} || [];
+       $sData = [$sData] if ref $sData ne 'ARRAY';
+    if (@$sData) {
+      my %tcols;
       for my $col (0 .. $#{$sData->[0]->{'c'}})
       {
-        push @tcol, $sData->[0]->{'c'}->[$col]->{'r'};
+        (my $key = $sData->[0]->{'c'}->[$col]->{'r'}) =~ s|\d+$||o;
+        $tcols{$key} = $col;
       }
-      $worksheet{'Columns'} = \@tcol;
+      $worksheet{'Columns'} = \%tcols;
       
       my(@trow);
       my(%tdata);
@@ -129,7 +109,7 @@ sub parse
                                    {Text=>$xCell->{'v'}};
           $xCellData->{Style}    = exists $xCell->{'s'} ? $styles->[$xCell->{'s'}] : undef;
           $xCellData->{Comments} = $sComments->{$xCell->{'r'}};
-          $xCellData->{DataType} = exists $xCell->{'t'} ? $self->{'CellDataTypes'}{$xCell->{'t'}} : undef;        
+          $xCellData->{DataType} = exists $xCell->{'t'} ? $CellDataTypes{$xCell->{'t'}} : undef;        
 
           push @rdata, $xCellData;
         }
@@ -149,6 +129,51 @@ sub parse
       
       $worksheet{'Rows'} = \@trow;
       $worksheet{'Data'} = \%tdata;
+    }
+
+
+    my $sMerge = $data->{'mergeCells'}->{'mergeCell'} || [];
+       $sMerge = [$sMerge] if ref $sMerge ne 'ARRAY';
+    if (@$sMerge)
+    {
+      my(%merge);
+      for my $mc (@{$sMerge})
+      {
+        my($from, $to) = split(':', $mc->{'ref'});
+        
+        $from =~ /([a-zA-Z]+)([0-9]+)/;
+        my($col1, $row1) = ($1, $2);
+        
+        $to =~ /([a-zA-Z]+)([0-9]+)/;
+        my($col2, $row2) = ($1, $2);
+
+        push @{$merge{$row1}},
+        {
+          'From' => { 'Row' => $row1, 'Column' => $col1 },
+          'To' => { 'Row' => $row2, 'Column' => $col2 }
+        };
+
+        my @rowNumbers  = ($row1 .. $row2);
+        my @cellLetters = ($col1 .. $col2);
+        my $dominateCell;
+        for my $rowNumber (@rowNumbers)
+        {
+          for my $colLetter (@cellLetters)
+          {
+            my $colNumber = $worksheet{'Columns'}{$colLetter};
+            my $cell = $worksheet{'Data'}{$rowNumber}{'Data'}[$colNumber];
+            $cell->{'Merged'} = 1;
+
+            $dominateCell ||= $cell;
+          }
+        }
+        $dominateCell->{'Merged'} = {};
+        $dominateCell->{'Merged'}{'Cols'} = scalar @cellLetters;
+        $dominateCell->{'Merged'}{'Rows'} = scalar @rowNumbers;
+        $dominateCell->{'Merged'}{'_cols'} = \@cellLetters;
+        $dominateCell->{'Merged'}{'_rows'} = \@rowNumbers;
+      }
+
       $worksheet{'Merge'} = \%merge;
     }
 
@@ -161,7 +186,7 @@ sub parse
   
   $worksheets{'Worksheets'} = \@sheetNames;
 
-  return \%worksheets;
+  return \%worksheets;;
 }
 
 sub getValues
@@ -374,7 +399,7 @@ sub getStyles
     {
       if (exists $xborders->[$ind]{$side}{'color'})
       {
-        $borders{$idx}{$side}{'color'} = $self->_indexedColors2RGB($xborders->[$ind]{$side}{'color'}{'indexed'});
+        $borders{$idx}{$side}{'color'} = $self->_color2RGB($xborders->[$ind]{$side}{'color'});
       }
       if (exists $xborders->[$ind]{$side}{'style'})
       {
@@ -392,8 +417,8 @@ sub getStyles
     $fills{$idx} = undef;
     if (exists $xfills->[$ind]{'patternFill'})
     {
-      $fills{$idx}{'bgColor'} = $xfills->[$ind]{'patternFill'}{'bgColor'}{'indexed'} ? $self->_indexedColors2RGB($xfills->[$ind]{'patternFill'}{'bgColor'}{'indexed'}) : undef;
-      $fills{$idx}{'fgColor'} = $xfills->[$ind]{'patternFill'}{'fgColor'}{'indexed'} ? $self->_indexedColors2RGB($xfills->[$ind]{'patternFill'}{'fgColor'}{'indexed'}) : undef;
+      $fills{$idx}{'bgColor'} = $self->_color2RGB($xfills->[$ind]{'patternFill'}{'bgColor'});
+      $fills{$idx}{'fgColor'} = $self->_color2RGB($xfills->[$ind]{'patternFill'}{'fgColor'});
     }
     
     $idx++;
@@ -489,7 +514,7 @@ sub _parseRunProperties
   $style->{'font-name'} = $rPr->{'rFont'}{'val'} if exists $rPr->{'rFont'};
   if (exists $rPr->{'color'} && $rPr->{'color'}{'indexed'})
   {
-    $style->{'color'} = $self->_indexedColors2RGB($rPr->{'color'}{'indexed'});
+    $style->{'color'} = $self->_color2RGB($rPr->{'color'});
   }
   unless ($style->{'color'})
   {
@@ -504,14 +529,235 @@ sub _parseRunProperties
   return $style;
 }
 
+sub _color2RGB
+{
+  my ($self, $color) = @_;
+
+  my $c = $color->{'indexed'} ? $self->_indexedColors2RGB($color->{'indexed'}) :
+          $color->{'rgb'}     ? $self->_argbColors2RGB($color->{'rgb'}) :
+          '';
+
+  return $c;
+}
+
 sub _indexedColors2RGB
 {
-  my $c = $_[0]->{'indexedColors2ARGB'}{$_[1]} || '';
+  my $c = $indexedColors2ARGB{$_[1]} || '';
      $c =~ s|^00||o;
   return $c;
 }
 
+sub _argbColors2RGB
+{
+  my $c = $_[1];
+     $c =~ s|^\w{2}||o;
+  return $c;
+}
+
+sub as_HTML
+{
+  #
+  # opts: tagName, containerTagName, notEmbedComment
+  #
+  my($self, $xfile, %opts) = @_;
+
+  my $xlsx = $self->parse($xfile);
+
+  my @tables;
+  foreach my $sheetName (@{$xlsx->{'Worksheets'}})
+  {
+    my $sheet = $xlsx->{$sheetName};
+    my @rows;
+
+    foreach my $rNum (@{$sheet->{'Rows'}})
+    {
+      my $rowData = $sheet->{'Data'}{$rNum} || {};
+      my @cells;
+
+      foreach my $cell (@{$rowData->{'Data'} || []})
+      {
+        my $cellHTML = $self->cell_as_HTML($cell, %opts);
+        if ($cellHTML ne '___merged___')
+        {
+          push @cells, $cellHTML;
+        }
+      }
+
+      push @rows, \@cells;
+    }
+    
+    if (@rows)
+    {
+      push @tables, \@rows;
+    }
+  }
+
+
+  my @html;
+  $opts{'containerTagName'} ||= !$opts{'tagName'} || $opts{'tagName'} eq 'td' ? 'tr' : 'div';
+  foreach my $table (@tables)
+  {
+    my @str;
+    for my $row (@$table)
+    {
+      push @str, '<'.$opts{'containerTagName'}.'>'.join('', @$row).'</'.$opts{'containerTagName'}.'>';
+    }
+    
+    my $html = join("\n", @str);
+    if ($opts{'containerTagName'} eq 'tr')
+    {
+      $html = '<table><tbody>'.$html.'</tbody></table>';
+    }
+    push @html, $html;
+  }
+
+  return wantarray ? @html : join("\n", @html);
+}
+
+sub cell_as_HTML
+{
+  #
+  # opts: tagName, notEmbedComment
+  #
+  my ($self, $cell, %opts) = @_;
+
+  $cell->{'Data'} ||= [];
+  $cell->{'Data'}   = [$cell->{'Data'}] if ref $cell->{'Data'} ne 'ARRAY';
+
+  my %tagAttrs;
+
+  if ($cell->{'Merged'})
+  {
+    if ($cell->{'Merged'} == 1)
+    {
+      return '___merged___';
+    }
+    else
+    {
+      $tagAttrs{'colspan'} = $cell->{'Merged'}{'Cols'} if $cell->{'Merged'}{'Cols'} > 1;
+      $tagAttrs{'rowspan'} = $cell->{'Merged'}{'Rows'} if $cell->{'Merged'}{'Rows'} > 1;
+    }
+  }
+
+  my @text;
+  foreach my $t (@{$cell->{'Data'} || []})
+  {
+    my %tStyle;
+
+    if ($t->{'Style'})
+    {
+      %tStyle = %{$t->{'Style'}};
+
+      $tStyle{'color'}            = '#'.$tStyle{'color'} if $tStyle{'color'} && $tStyle{'color'} !~ /^#/;
+      $tStyle{'background-color'} = '#'.$tStyle{'background-color'} if $tStyle{'background-color'} && $tStyle{'background-color'} !~ /^#/;
+      $tStyle{'font-weight'}      = 'bold'         if delete $tStyle{'bold'};
+      $tStyle{'font-style'}       = 'italic'       if delete $tStyle{'italic'};
+      $tStyle{'text-decoration'}  = 'line-through' if delete $tStyle{'strike'};
+      $tStyle{'font-size'}       .= 'pt'           if $tStyle{'font-size'} !~ /pt$/;
+
+      if (my $font = delete $tStyle{'font-name'})
+      {
+        $tStyle{'font-family'} = $font;
+      }
+
+      if ($cell->{Style})
+      {
+        if (($cell->{'Style'}{'font-weight'} || $cell->{'Style'}{'bold'}) && !$tStyle{'font-weight'})
+        {
+          # psevdo-Default font-weight
+          $tStyle{'font-weight'} = 'normal';
+        }
+        if (($cell->{'Style'}{'font-style'} || $cell->{'Style'}{'italic'}) && !$tStyle{'font-style'})
+        {
+          # psevdo-Default font-style
+          $tStyle{'font-style'} = 'normal';
+        }
+        if (($cell->{'Style'}{'text-decoration'} || $cell->{'Style'}{'strike'}) && !$tStyle{'text-decoration'})
+        {
+          # psevdo-Default text-decoration
+          $tStyle{'text-decoration'} = 'none';
+        }
+      }
+    }
+
+    my $tVal = $t->{'Text'} || '';
+    if (%tStyle)
+    {
+      $tVal = '<span style="'.join(';', map {$_.':'.$tStyle{$_}} keys %tStyle).'">'.$tVal.'</span>';
+    }
+
+    push @text, $tVal;
+  }
+  
+  my $cellText = @text ? join("\n", @text) : '';
+  
+  if ($cell->{'Style'} && $cellText =~ /\S/o)
+  {
+    #
+    # no sense to write node style if content is empty
+    #
+
+    $cell->{'Style'}{'color'}            = '#'.$cell->{'Style'}{'color'} if $cell->{'Style'}{'color'} && $cell->{'Style'}{'color'} !~ /^#/;
+    $cell->{'Style'}{'background-color'} = '#'.$cell->{'Style'}{'background-color'} if $cell->{'Style'}{'background-color'} && $cell->{'Style'}{'background-color'} !~ /^#/;
+    $cell->{'Style'}{'font-style'}       = 'italic'       if delete $cell->{'Style'}{'italic'};
+    $cell->{'Style'}{'font-weight'}      = 'bold'         if delete $cell->{'Style'}{'bold'};
+    $cell->{'Style'}{'text-decoration'}  = 'line-through' if delete $cell->{'Style'}{'strike'};
+    $cell->{'Style'}{'font-size'}       .= 'pt'           if $cell->{'Style'}{'font-size'} !~ /pt$/;
+
+    if (my $font = delete $cell->{'Style'}{'font-name'})
+    {
+      $cell->{'Style'}{'font-family'} = $font;
+    }
+    
+    if ($cell->{'Style'}{'border'} && ref $cell->{'Style'}{'border'})
+    {
+      my $border = delete $cell->{'Style'}{'border'};
+      for my $b (keys %$border)
+      {
+        $cell->{'Style'}{"border-$b"} = join ' ', '1px', 
+                                                  ($border->{$b}{'style'} && exists $HtmlBorderStyles{$border->{$b}{'style'}} ? $HtmlBorderStyles{$border->{$b}{'style'}} : 'solid'),
+                                                  ($border->{$b}{'color'} ? '#'.$border->{$b}{'color'} : 'black');
+      }
+      
+      if ($cell->{'Style'}{'border-top'} eq $cell->{'Style'}{'border-right'} && $cell->{'Style'}{'border-top'} eq $cell->{'Style'}{'border-bottom'} && $cell->{'Style'}{'border-top'} eq $cell->{'Style'}{'border-left'})
+      {
+        #
+        # merge border if it possible
+        #
+        $cell->{'Style'}{'border'} = $cell->{'Style'}{'border-top'};
+        for my $b (keys %$border)
+        {
+          delete $cell->{'Style'}{"border-$b"};
+        }
+      }
+    }
+    
+    $tagAttrs{style} = join(';', map {$_.':'.$cell->{'Style'}{$_}} keys %{$cell->{'Style'}});
+  }
+
+  my $comment = join "\n", @{$cell->{'Comments'}{'Text'} || []};
+  $comment =~ s|\s+$||gmso;
+  if ($comment =~ /\S/o && !$opts{'notEmbedComment'})
+  {
+    $comment =~ s|"|\"|go;
+    $comment =~ s|>|&gt;|go;
+    $comment =~ s|<|&lt;|go;
+
+    $tagAttrs{'title'} = $comment;
+    $tagAttrs{'class'} = 'commented';
+  }
+
+
+  my $tagName = $opts{'tagName'} || 'td';
+  
+  my $cellHTML = '<'.$tagName.' '.join(' ', map {qq|$_="$tagAttrs{$_}"|} grep {$tagAttrs{$_} =~ /\S/o} keys %tagAttrs).'>'.$cellText.'</'.$tagName.'>';
+  
+  return wantarray ? ($cellHTML, $comment) : $cellHTML;
+}
+
+
 1;
+
 __END__
 
 =head1 NAME
